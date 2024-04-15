536b336912d0cf8b8936f523e947b2237da7a5e8
isglicenses <isg.licenses@visionaustralia.org>
2024-04-15 17:16:20 +0530

    step 1 final for register participant

---
+++ assets/js/custom.js
@@ -0,0 +1,25 @@
+let currentStep = 1;
+const steps = document.querySelectorAll(".step");
+const stepContents = document.querySelectorAll(".step-content");
+const nextBtn = document.getElementById("nextBtn");
+
+function showStep(stepNumber) {
+  steps.forEach((step) => {
+    step.classList.remove("active");
+  });
+  stepContents.forEach((content) => {
+    content.classList.add("d-none");
+  });
+  steps[stepNumber - 1].classList.add("active");
+  stepContents[stepNumber - 1].classList.remove("d-none");
+  updateButtonVisibility(stepNumber);
+}
+
+function nextStep() {
+  if (currentStep < steps.length) {
+    currentStep++;
+    showStep(currentStep);
+  }
+}
+
+showStep(currentStep);
